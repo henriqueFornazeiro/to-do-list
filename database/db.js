b24d0a875bc8c3a050f4e1b61b0042352518f5b6
Henrique Fornazeiro <henrique.fornazeiro@hotmail.com>
2023-01-27 17:39:27 -0300

    configurações para deploy

--- database/db.js
+++ database/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectToDb = () => {
   mongoose
     .connect(
-      "mongodb+srv://root:JG11GB2dW1OX4Qem@todolist.jnscjdw.mongodb.net/?retryWrites=true&w=majority",
+      process.env.DB_URI,
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
--- index.js
+++ index.js
@@ -1,15 +1,16 @@
+require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const routes = require("./routes/routes");
-const connectToDb = require("./database/db")
+const connectToDb = require("./database/db");
 
-connectToDb()
+connectToDb();
 const app = express();
-const port = 4000;
+const port = process.env.PORT;
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
-app.use(express.urlencoded())
+app.use(express.urlencoded());
 app.use(routes);
 
 app.listen(port, () => {
